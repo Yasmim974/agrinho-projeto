4fd6a748e5c13db17b73f744402b8bb3a56ff0c0
yasmim almeida <yasmim.oliveira.almeida@escola.pr.gov.br>
2026-06-11 10:41:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+// Formulário de contato
+const form = document.getElementById('formContato');
+const resposta = document.getElementById('resposta');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); // evita envio real
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    if(nome && email && mensagem) {
+        resposta.textContent = `Obrigado, ${nome}! Sua mensagem foi recebida.`;
+        resposta.style.color = 'green';
+        form.reset();
+    } else {
+        resposta.textContent = "Por favor, preencha todos os campos.";
+        resposta.style.color = 'red';
+    }
+});
